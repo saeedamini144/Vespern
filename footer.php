d4330795461aa8f26cc14376bf0e624144357cf8
saeed144 <saaed.amini@gmail.com>
2024-08-27 16:35:07 +0330

    change back to top link icon

--- footer.php
+++ footer.php
@@ -82,7 +82,7 @@
                          <!-- You can delete the links only if you purchased the pro version. -->
                          <!-- Licensing information: https://bootstrapmade.com/license/ -->
                          <!-- Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/vesperr-free-bootstrap-template/ -->
-                         Designed by <a href="">Saeed Amini</a>
+                         Designed by <a href="mailto:saaed.amini@gmail.com">Saeed Amini</a>
                      </div>
                  </div>
                  <div class="col-lg-6">
@@ -98,7 +98,7 @@
      </section>
  </footer><!-- End Footer -->
 
- <a href="saaed.amini@gmail.com" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
+ <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
  <?php
     wp_footer();
     ?>
