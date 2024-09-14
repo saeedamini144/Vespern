c315eb7d68ba8e53e5ed5013ad97be236ad9aac9
saeed144 <saaed.amini@gmail.com>
2024-09-14 16:36:16 +0330

    display none when no content add

--- template-parts/cardservices-service.php
+++ template-parts/cardservices-service.php
@@ -1,7 +1,27 @@
-<div class="col-md-6 col-lg-3 d-flex align-items-stretch mb-5 mb-lg-0">
-    <div class="icon-box" data-aos="fade-up" data-aos-delay="100">
-        <div class="icon"><i class="bx bxl-dribbble"></i></div>
-        <h4 class="title"><a href="">Lorem Ipsum</a></h4>
-        <p class="description">Voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi</p>
-    </div>
-</div>
+<?php
+$services_section_card = fw_get_db_customizer_option('services_section_card');
+?>
+<?php foreach ($services_section_card as $card_services) {
+    if (!empty($services_section_card)) {
+?>
+        <div class="col-md-6 col-lg-3 d-flex align-items-stretch mb-5 mb-lg-0">
+            <div class="icon-box" data-aos="fade-up" data-aos-delay="100">
+                <div class="icon"><i class="<?php echo $card_services['services_icon'] ?>"></i></div>
+                <h4 class="title"><a href=""><?php echo $card_services['services_title'] ?></a></h4>
+                <p class="description"><?php echo $card_services['services_card_desc'] ?></p>
+            </div>
+        </div>
+    <?php
+    } else { //add display none class when user no content add
+    ?>
+        <div class="d-none col-md-6 col-lg-3 d-flex align-items-stretch mb-5 mb-lg-0">
+            <div class="icon-box" data-aos="fade-up" data-aos-delay="100">
+                <div class="icon"><i class="<?php echo $card_services['services_icon'] ?>"></i></div>
+                <h4 class="title"><a href=""><?php echo $card_services['services_title'] ?></a></h4>
+                <p class="description"><?php echo $card_services['services_card_desc'] ?></p>
+            </div>
+        </div>
+<?php
+    }
+}
+?>
