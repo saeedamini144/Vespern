a3ae3e952c7c0fdd3c735787e1e2e7ad1fd4855f
saeed144 <saaed.amini@gmail.com>
2024-09-15 11:22:27 +0330

    add portfolio card for main page

---
+++ template-parts/portfolio-card.php
@@ -0,0 +1,9 @@
+<div class="col-md-6 d-flex align-items-stretch">
+    <div class="card" style='background-image: url("<?php bloginfo('template_url'); ?>/assets/img/more-services-1.jpg");' data-aos="fade-up" data-aos-delay="100">
+        <div class="card-body">
+            <h5 class="card-title"><a href="">Lobira Duno</a></h5>
+            <p class="card-text">Lorem ipsum dolor sit amet, consectetur elit, sed do eiusmod tempor ut labore et dolore magna aliqua.</p>
+            <div class="read-more"><a href="#"><i class="bi bi-arrow-right"></i> Read More</a></div>
+        </div>
+    </div>
+</div>
