af71914640b3bb7397ed3f8bee39616d6622df8e
saeed144 <saaed.amini@gmail.com>
2024-09-15 11:23:04 +0330

    corect the services show when is no content

--- template-parts/cardservices-service.php
+++ template-parts/cardservices-service.php
@@ -4,7 +4,7 @@ $services_section_card = fw_get_db_customizer_option('services_section_card');
 <?php foreach ($services_section_card as $card_services) {
     if (!empty($services_section_card)) {
 ?>
-        <div class="col-md-6 col-lg-3 d-flex align-items-stretch mb-5 mb-lg-0">
+        <div class="col-md-6 col-lg-3 d-flex align-items-stretch mb-5 mb-lg-3">
             <div class="icon-box" data-aos="fade-up" data-aos-delay="100">
                 <div class="icon"><i class="<?php echo $card_services['services_icon'] ?>"></i></div>
                 <h4 class="title"><a href=""><?php echo $card_services['services_title'] ?></a></h4>
@@ -14,7 +14,7 @@ $services_section_card = fw_get_db_customizer_option('services_section_card');
     <?php
     } else { //add display none class when user no content add
     ?>
-        <div class="d-none col-md-6 col-lg-3 d-flex align-items-stretch mb-5 mb-lg-0">
+        <div class="d-none col-md-6 col-lg-3 d-flex align-items-stretch mb-5 mb-lg-3">
             <div class="icon-box" data-aos="fade-up" data-aos-delay="100">
                 <div class="icon"><i class="<?php echo $card_services['services_icon'] ?>"></i></div>
                 <h4 class="title"><a href=""><?php echo $card_services['services_title'] ?></a></h4>
