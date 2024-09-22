418fa7ddbcb292f01a8590df2789f03a8c2d3fae
saeed144 <saaed.amini@gmail.com>
2024-09-22 15:12:48 +0330

    add new file for price card

---
+++ template-parts/price-card.php
@@ -0,0 +1,16 @@
+<div class="col-lg-4 col-md-6">
+    <div class="box" data-aos="zoom-in-right" data-aos-delay="200">
+        <h3>Free</h3>
+        <h4><sup>$</sup>0<span> / month</span></h4>
+        <ul>
+            <li>Aida dere</li>
+            <li>Nec feugiat nisl</li>
+            <li>Nulla at volutpat dola</li>
+            <li class="na">Pharetra massa</li>
+            <li class="na">Massa ultricies mi</li>
+        </ul>
+        <div class="btn-wrap">
+            <a href="#" class="btn-buy">Buy Now</a>
+        </div>
+    </div>
+</div>
